a4a5a1054171ab2de00a806452d16db7443bcc36
denpalrius <denpalrius@gmail.com>
2020-07-06 04:15:47 +0300

    set up clam scan

--- function-scantrigger-node/index.js
+++ function-scantrigger-node/index.js
@@ -1,20 +1,4 @@
-/*
-* Copyright 2019 Google LLC
-
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-
-*     https://www.apache.org/licenses/LICENSE-2.0
-
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-const request = require('request-promise');
+const Axios = require('axios').default;
 
 /**
  * Background Cloud Function that handles the 'google.storage.object.finalize'
@@ -27,30 +11,30 @@ const request = require('request-promise');
 exports.requestMalwareScan = async (data, context) => {
 
   const file = data;
-  console.log(`  Event ${context.eventId}`);
+
+  console.log(`  Event : ${context.eventId}`);
   console.log(`  Event Type: ${context.eventType}`);
   console.log(`  Bucket: ${file.bucket}`);
   console.log(`  File: ${file.name}`);
 
-  let options = {
-    method: 'POST',
-    uri: process.env.SCAN_SERVICE_URL,
-    body: {
-      location: `gs://${file.bucket}/${file.name}`,
-      filename: file.name,
-      bucketname: file.bucket
-    },
-    json: true
-  }
+  const url = process.env.SCAN_SERVICE_URL;
 
   try {
-    if(context.eventType === "google.storage.object.finalize") {
-      await request(options);
+    if (context.eventType === "google.storage.object.finalize") {
+
+      const res = await Axios.post(url, {
+        location: `gs://${file.bucket}/${file.name}`,
+        filename: file.name,
+        bucketname: file.bucket
+      });
+
       console.log(`Malware scan succeeded for: ${file.name}`);
+      console.log(`Malware scan results:: ${res.data}`);
+
     } else {
       console.log('Malware scanning is only invoked when documents are uploaded or updated');
     }
-  } catch(e) {
-    console.error(`Error occurred while scanning ${file.name}`, e);
+  } catch (e) {
+    console.error(`Error occurred while scanning ${file.name}`, e.data ? e.data : String(e));
   }
 }
